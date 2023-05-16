f983e64f0d5ee6930455f350fb5843c38f55d8fb
alfred <alfredng019@gmail.com>
2023-05-16 09:49:50 -0400

    Added imports to App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 
 export default function App() {
   return (
