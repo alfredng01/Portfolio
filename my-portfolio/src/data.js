10d8be7fbdb14fd13ce307b39ccb30425e3602df
alfred <alfredng019@gmail.com>
2023-05-16 14:32:15 -0400

    Updated projects

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -1,19 +1,19 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "C++ Text Editor",
+    subtitle: "MVC and C++",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
+      "A C++ VI Style Text Editor",
+    image: "./editor.png",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Portfolio Website",
+    subtitle: "React and Tailwind CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "The Site you are on now!",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/alfredng01/Portfolio",
   },
 ];
 
